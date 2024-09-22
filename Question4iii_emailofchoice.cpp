6ef37c38a6b1fb56e9d574d46ddfcdb3954cdf5d
emmanuelotech <150592072+emmanuelotech@users.noreply.github.com>
2024-09-22 12:43:43 +0300

    Create Question4iii_emailofchoice.cpp

---
+++ Question4iii_emailofchoice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 3;
+    char email[]="emmanuelokello2050@vdtw2.onmicrosoft.com";
+
+   
+
+    cout << "email: " << email << endl;
+    
+    return 0;
+}
